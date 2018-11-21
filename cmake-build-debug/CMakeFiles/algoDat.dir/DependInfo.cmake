6575b44239c7ec28189c6752f581d8f28f007aa4
DerAlexx <alexweb199@hotmail.de>
2018-11-21 22:34:04 +0100

    s

--- cmake-build-debug/CMakeFiles/algoDat.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/algoDat.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/deralex/Schreibtisch/jj/main.cpp" "/home/deralex/Schreibtisch/jj/cmake-build-debug/CMakeFiles/algoDat.dir/main.cpp.o"
+  "/home/deralex/Schreibtisch/finalalgo/main.cpp" "/home/deralex/Schreibtisch/finalalgo/cmake-build-debug/CMakeFiles/algoDat.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
